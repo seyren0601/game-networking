c4793f45c22a128fcfc2c82a03376d9be8adb550
ThinhNguyen <sorrenw@gmail.com>
2025-07-20 21:11:28 +0700

    Init commit, created SocketAddress wrapper for sockaddr and sockaddr_in

---
+++ chapter_3/Creating_Sockets/Creating_Sockets.cpp
@@ -0,0 +1,60 @@
+﻿// Creating_Sockets.cpp : Defines the entry point for the application.
+//
+
+#include "SocketAddress.h"
+#include "Creating_Sockets.h"
+#include <WinSock2.h>
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	WSAData data;
+	int startupResult = WSAStartup(MAKEWORD(2, 2), &data);
+	if (startupResult != 0) {
+		cerr << "WSAStartup failed with error: " << SOCKET_ERROR << endl;
+		cerr << "Last error code: " << WSAGetLastError() << endl;
+		return 1;
+	}
+
+
+	// Manually socket creation using WinSock API
+	SOCKET udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	closesocket(udpSocket);
+
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(5000);
+	serverAddress.sin_addr.S_un.S_un_b.s_b1 = 192;
+	serverAddress.sin_addr.S_un.S_un_b.s_b2 = 168;
+	serverAddress.sin_addr.S_un.S_un_b.s_b3 = 1;
+	serverAddress.sin_addr.S_un.S_un_b.s_b4 = 1;
+	memset(&serverAddress.sin_zero, 0, sizeof(serverAddress.sin_zero));
+
+	cout << "Server address: " << static_cast<int>(serverAddress.sin_addr.S_un.S_un_b.s_b1) << "."
+		 << static_cast<int>(serverAddress.sin_addr.S_un.S_un_b.s_b1) << "."
+		 << static_cast<int>(serverAddress.sin_addr.S_un.S_un_b.s_b2) << "."
+		<< static_cast<int>(serverAddress.sin_addr.S_un.S_un_b.s_b3) << endl;
+
+	// Create a SocketAddress object
+	SocketAddressPtr socketAddress = make_shared<SocketAddress>("192.168.1.2", 6000);
+
+	cout << "SocketAddress created with address: "
+		 << static_cast<int>(socketAddress->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b1) << "."
+		 << static_cast<int>(socketAddress->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b2) << "."
+		 << static_cast<int>(socketAddress->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b3) << "."
+		 << static_cast<int>(socketAddress->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b4)
+		<< " and port: " << ntohs(socketAddress->GetAsSockAddrIn()->sin_port) << endl;
+
+
+
+	int closeResult = WSACleanup();
+	if (closeResult != 0) {
+		cerr << "WSACleanup failed with error: " << SOCKET_ERROR << endl;
+		cerr << "Last error code: " << WSAGetLastError() << endl;
+		return 1;
+	}
+	return 0;
+}
---
+++ chapter_3/Creating_Sockets/SocketAddress.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <cstdint>
+#include <Winsock2.h>
+#include <memory>
+#include <ws2tcpip.h>
+
+using namespace std;
+
+class SocketAddress {
+public:
+	SocketAddress(uint32_t inAddress, uint16_t inPort) {
+		GetAsSockAddrIn()->sin_family = AF_INET;
+		GetAsSockAddrIn()->sin_port = htons(inPort);
+		GetAsSockAddrIn()->sin_addr.S_un.S_addr = htonl(inAddress);
+		memset(&GetAsSockAddrIn()->sin_zero, 0, sizeof(GetAsSockAddrIn()->sin_zero));
+	};
+
+	SocketAddress(const sockaddr& inSockAddr) {
+		memcpy(&mSockAddr, &inSockAddr, sizeof(sockaddr));
+	};
+
+	SocketAddress(const char* inAddress, uint16_t inPort) {
+		GetAsSockAddrIn()->sin_family = AF_INET;
+		GetAsSockAddrIn()->sin_port = htons(inPort);
+		inet_pton(AF_INET, inAddress, &GetAsSockAddrIn()->sin_addr);
+		memset(&GetAsSockAddrIn()->sin_zero, 0, sizeof(GetAsSockAddrIn()->sin_zero));
+	};
+
+	size_t GetSize() const { return sizeof(sockaddr); }
+	sockaddr_in* GetAsSockAddrIn() { return reinterpret_cast<sockaddr_in*>(&mSockAddr); }
+
+private:
+	sockaddr mSockAddr;
+};
+
+typedef shared_ptr<SocketAddress> SocketAddressPtr;
