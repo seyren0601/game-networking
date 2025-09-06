5123609bb52ad8107501af0f47f5b503e897d410
ThinhNguyen <sorrenw@gmail.com>
2025-09-06 20:11:49 +0700

    Remove manual address creation and redundant codes

--- chapter_3/Creating_Sockets/Creating_Sockets.cpp
+++ chapter_3/Creating_Sockets/Creating_Sockets.cpp
@@ -19,39 +19,9 @@ int main()
 		return 1;
 	}
 
-
-	// Manually socket creation using WinSock API
-	SOCKET udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
-	closesocket(udpSocket);
-
-	sockaddr_in serverAddress;
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(5000);
-	serverAddress.sin_addr.S_un.S_un_b.s_b1 = 192;
-	serverAddress.sin_addr.S_un.S_un_b.s_b2 = 168;
-	serverAddress.sin_addr.S_un.S_un_b.s_b3 = 1;
-	serverAddress.sin_addr.S_un.S_un_b.s_b4 = 1;
-	memset(&serverAddress.sin_zero, 0, sizeof(serverAddress.sin_zero));
-
-	cout << "Server address: " << static_cast<int>(serverAddress.sin_addr.S_un.S_un_b.s_b1) << "."
-		 << static_cast<int>(serverAddress.sin_addr.S_un.S_un_b.s_b1) << "."
-		 << static_cast<int>(serverAddress.sin_addr.S_un.S_un_b.s_b2) << "."
-		<< static_cast<int>(serverAddress.sin_addr.S_un.S_un_b.s_b3) << endl;
-
-	// Create a SocketAddress object
-	SocketAddressPtr socketAddress = make_shared<SocketAddress>("192.168.1.2", 6000);
-
-	cout << "SocketAddress created with address: "
-		 << static_cast<int>(socketAddress->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b1) << "."
-		 << static_cast<int>(socketAddress->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b2) << "."
-		 << static_cast<int>(socketAddress->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b3) << "."
-		 << static_cast<int>(socketAddress->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b4)
-		<< " and port: " << ntohs(socketAddress->GetAsSockAddrIn()->sin_port) << endl;
-
 	// Create a SocketAddress using the factory method
 	SocketAddressFactory factory;
-	auto futureSocket = factory.CreateIPv4FromString("192.168.1.3:7000");
-	SocketAddressPtr factoryCreatedSocket = futureSocket.get();
+	auto factoryCreatedSocket = factory.CreateIPv4FromString("192.168.1.3:7000");
 	cout << "Factory created SocketAddress with address: "
 		 << static_cast<int>(factoryCreatedSocket->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b1) << "."
 		 << static_cast<int>(factoryCreatedSocket->GetAsSockAddrIn()->sin_addr.S_un.S_un_b.s_b2) << "."
